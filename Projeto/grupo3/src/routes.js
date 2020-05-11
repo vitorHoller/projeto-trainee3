b925c0e0805a1f58a4921d11468ac42746bc8e41
vitor <vitor.holler@outlook.pt>
2020-05-11 15:33:18 -0300

    App.js, routes atualizado e index atualizado

--- Projeto/grupo3/src/routes.js
+++ Projeto/grupo3/src/routes.js
@@ -1,19 +1,22 @@
 import React from 'react'
-import CadastroMidias from './Pages/CadastroMidias'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import Login from './Pages/Login/Login'
+{/*
 import CadastroUsuario from './Pages/CadastroUsuario'
 import ListarMidias from './Pages/ListarMidias'
-import ListarMidiasAddPeloUsuario from './Pages/ListarMidiasAddPeloUsuario'
-import Login from './Pages/Login'
+import ListarMidiasAddPeloUsuario from './Pages/ListarMidiasAddPeloUsuario/listarMidiasAddPeloUsuario'
+*/}
+
 
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/cadastromidias" exact component={CadastroMidias} />
-                <Route path="/cadastrousuario" exact component={CadastroUsuario} />
+               {/* <Route path="/cadastrousuario" exact component={CadastroUsuario} />
                 <Route path="/listarmidias" exact component={ListarMidias} />
-                <Route path="/listarmidiasusuario" exact component={ListarMidiasAddPeloUsuario} />
+                <Route path="/listarmidiasusuario" exact component={ListarMidiasAddPeloUsuario} /> */}
                 <Route path="/login" exact component={Login} />
             </Switch>
         </BrowserRouter>
