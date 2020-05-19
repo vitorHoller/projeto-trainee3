5c2b8f5a88c8b63deb03a7da35ef2cc729a6b96e
Antonio Britto <antonio.britto.2000@hotmail.com>
2020-05-19 16:16:46 -0300

    Inclusão do ícone de login do lado de cadastre-se

--- Projeto/grupo3/src/Pages/Login/Login.js
+++ Projeto/grupo3/src/Pages/Login/Login.js
@@ -1,28 +1,29 @@
 import React, { useState } from 'react'
 import './Login.css'
-import {Button, Nav, NavDropdown, Navbar, FormControl} from 'react-bootstrap'
+import { Button, Nav, NavDropdown, Navbar, FormControl } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { useHistory } from 'react-router-dom'
+import { FiLogIn } from "react-icons/fi"
 
 export default function Login() {
     const usuario = 'Grupo 3'
     const senha = 'Grupo3';
 
-    const [username,setUserName] = useState('');
-    const [password,setPassword] = useState('');
+    const [username, setUserName] = useState('');
+    const [password, setPassword] = useState('');
 
     const history = useHistory();
 
-    function handleSubmit(){
-        
+    function handleSubmit() {
+
         if (username === usuario && password === senha) {
             history.push('cadastromidia')
         }
-        else{
-            if (username !== usuario){
+        else {
+            if (username !== usuario) {
                 alert('Usuário inválido');
             }
-            else if(password !== senha){
+            else if (password !== senha) {
                 alert('Senha Inválida');
             }
         }
@@ -35,17 +36,20 @@ export default function Login() {
             </div>
             <div className='body'>
                 <div className='box'>
-                    <h1 className = 'title'>Eu já vi isso !</h1>
+                    <h1 className='title'>Eu já vi isso !</h1>
                     <div className='login'>
                         <input className='input' placeholder='Usuário' onChange={(e) => setUserName(e.target.value)} ></input>
-                        <input className='input' placeholder='Senha' type = 'password' onChange={(e) => setPassword(e.target.value)} ></input>
-                        <a className = 'cadastro' href='http://localhost:3000/cadastrousuario'>Cadastre-se</a>
-                        <Button className = 'button' variant = 'secondary' type="submit" onClick={() => handleSubmit()}>Entrar</Button>{' '}
+                        <input className='input' placeholder='Senha' type='password' onChange={(e) => setPassword(e.target.value)} ></input>
+                        <a className='cadastro' href='http://localhost:3000/cadastrousuario'>
+                            <FiLogIn size={16} />
+                            Cadastre-se
+                        </a>
+                        <Button className='button' variant='secondary' type="submit" onClick={() => handleSubmit()}>Entrar</Button>{' '}
                     </div>
                 </div>
 
-                <div className = 'rodape'>
-                 <p>Desenvolvido por Grupo 3</p>   
+                <div className='rodape'>
+                    <p>Desenvolvido por Grupo 3</p>
                 </div>
 
             </div>
