fa6e7cb8ced9658571486bd02efd6c6d5080fb4f
Vitor <vitor.holler@outlook.pt>
2020-05-19 17:34:51 -0300

    Mudanças na tabela de listar mídias

--- Projeto/grupo3/src/Pages/ListarMidias/listarMidias.js
+++ Projeto/grupo3/src/Pages/ListarMidias/listarMidias.js
@@ -1,173 +1,103 @@
 import React from 'react'
 import './listarMidias.css'
-import { Button, Form, FormGroup, FormLabel, Nav, NavDropdown, Navbar, FormControl } from 'react-bootstrap'
+import { Table, Nav, NavDropdown, Navbar, FormControl, Modal, Button } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+function MyVerticallyCenteredModal(props) {
+    return (
+        <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    La Casa de Papel
+          </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <h4>Quantidade de Episódios</h4>
+                <p>38</p>
+                <h4>Gênero</h4>
+                <p>Suspense</p>
+                <h4>Data de Lançamento</h4>
+                <p>01/01/01</p>
+                <h4>Tempo de Duração</h4>
+                <p>45 minutos</p>
+                <h4>Descrição</h4>
+                <p>Série muito doida!</p>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button>Editar</Button>
+                <Button onClick={props.onHide}>Close</Button>
+            </Modal.Footer>
+        </Modal>
+    );
+}
 
 export default function ListarMidias() {
 
+    const [modalShow, setModalShow] = React.useState(false);
     return (
 
-        <div className='back'>
+        <div className='back overflow-hidden'>
 
-          <div className='header rounded' >
-                <Navbar className = 'batba rounded mt-2 mr-2' bg="light" expand="lg"> 
+            <div className='headerr rounded'>
+                <Navbar className='batba rounded mt-2 mr-2' bg="light" expand="lg">
                     <Navbar.Brand>Eu já vi isso!!</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
                             <Nav.Link href='http://localhost:3000/'>Login</Nav.Link>
                             <Nav.Link href='http://localhost:3000/cadastrousuario'>Cadastrar Usuário</Nav.Link>
-                            <Nav.Link href="http://localhost:3000/midias">Listar Mídias</Nav.Link>
+                            <Nav.Link href="http://localhost:3000/midias">Cadastrar Mídias</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Navbar>
             </div>
 
-            <div className='body'>
+            <div className = 'mt-3'>
 
-                <h1>Mídias cadastradas</h1>
-                <ul>
+                <h1 className='titulo p-3 pr-5 pl-5 rounded ml-auto mr-auto m-3 mb-4'>Mídias cadastradas</h1>
 
-                    <div className='bordaporfora'>
-                        {/*numeros de 'bloquinhos' que vai ser mostrado na pagina
-                    depende da quantidade de midias inseridas que o backend passar.*/}
-                        <li>
-                            <strong>Nome da mídia</strong>
-                            <p>Mídia teste</p>
+                <div className = 'tabelaheight mr-auto ml-auto'>
 
-                            <strong>Tipo da mídia</strong>
-                            <p>Tipo da mídia teste</p>
+                    <Table striped bordered variant='dark'>
+                        <thead className='titulotabela text-center' >
+                            <tr className='text-body'>
+                                <th>#</th>
+                             <th>Nome da Midia</th>
+                             <th>Tipo de Mídia</th>
+                             <th>Detalhes da Mídia</th>
+                             </tr>
+                        </thead>
 
-                            <strong>Quantidade de episódios</strong>
-                            <p>Quantidade de episódios teste</p>
-
-                            <strong>Gênero</strong>
-                            <p>Gênero teste</p>
-
-                            <strong>Data de lançamento</strong>
-                            <p>Data de lançamento teste</p>
-
-                            <strong>Tempo de duração</strong>
-                            <p>Tempo de duração teste</p>
-                        </li>
-                    </div>
-
-                    <div className='bordaporfora'>
-                        <li>
-                            <strong>Nome da mídia</strong>
-                            <p>Mídia teste</p>
-
-                            <strong>Tipo da mídia</strong>
-                            <p>Tipo da mídia teste</p>
-
-                            <strong>Quantidade de episódios</strong>
-                            <p>Quantidade de episódios teste</p>
-
-                            <strong>Gênero</strong>
-                            <p>Gênero teste</p>
-
-                            <strong>Data de lançamento</strong>
-                            <p>Data de lançamento teste</p>
-
-                            <strong>Tempo de duração</strong>
-                            <p>Tempo de duração teste</p>
-                        </li>
-                    </div>
-
-                    <div className='bordaporfora'>
-                        <li>
-                            <strong>Nome da mídia</strong>
-                            <p>Mídia teste</p>
-
-                            <strong>Tipo da mídia</strong>
-                            <p>Tipo da mídia teste</p>
-
-                            <strong>Quantidade de episódios</strong>
-                            <p>Quantidade de episódios teste</p>
-
-                            <strong>Gênero</strong>
-                            <p>Gênero teste</p>
-
-                            <strong>Data de lançamento</strong>
-                            <p>Data de lançamento teste</p>
-
-                            <strong>Tempo de duração</strong>
-                            <p>Tempo de duração teste</p>
-                        </li>
-                    </div>
-
-                    <div className='bordaporfora'>
-                        <li>
-                            <strong>Nome da mídia</strong>
-                            <p>Mídia teste</p>
-
-                            <strong>Tipo da mídia</strong>
-                            <p>Tipo da mídia teste</p>
-
-                            <strong>Quantidade de episódios</strong>
-                            <p>Quantidade de episódios teste</p>
-
-                            <strong>Gênero</strong>
-                            <p>Gênero teste</p>
-
-                            <strong>Data de lançamento</strong>
-                            <p>Data de lançamento teste</p>
-
-                            <strong>Tempo de duração</strong>
-                            <p>Tempo de duração teste</p>
-                        </li>
-                    </div>
-
-                    <div className='bordaporfora'>
-                        <li>
-                            <strong>Nome da mídia</strong>
-                            <p>Mídia teste</p>
-
-                            <strong>Tipo da mídia</strong>
-                            <p>Tipo da mídia teste</p>
-
-                            <strong>Quantidade de episódios</strong>
-                            <p>Quantidade de episódios teste</p>
-
-                            <strong>Gênero</strong>
-                            <p>Gênero teste</p>
-
-                            <strong>Data de lançamento</strong>
-                            <p>Data de lançamento teste</p>
-
-                            <strong>Tempo de duração</strong>
-                            <p>Tempo de duração teste</p>
-                        </li>
-                    </div>
-
-                    <div className='bordaporfora'>
-                        <li>
-                            <strong>Nome da mídia</strong>
-                            <p>Mídia teste</p>
-
-                            <strong>Tipo da mídia</strong>
-                            <p>Tipo da mídia teste</p>
-
-                            <strong>Quantidade de episódios</strong>
-                            <p>Quantidade de episódios teste</p>
-
-                            <strong>Gênero</strong>
-                            <p>Gênero teste</p>
-
-                            <strong>Data de lançamento</strong>
-                            <p>Data de lançamento teste</p>
-
-                            <strong>Tempo de duração</strong>
-                            <p>Tempo de duração teste</p>
-                        </li>
-                    </div>
-                </ul>
+                        <tbody className='bodytabela text-center text-dark'>
+                             <tr>
+                                 <td className='numero pt-3'>1</td>
+                                 <td className='pt-3'>La Casa de Papel</td>
+                                 <td className='pt-3'>Série</td>
+                                 <>
+                                 <Button variant="outline-light mt-3" className='text-white bg-dark w-75' onClick={() => setModalShow(true)}>Clique Aqui</Button>
+                                 <MyVerticallyCenteredModal
+                                 show={modalShow}
+                                 onHide={() => setModalShow(false)}
+                                 />
+                                 </>
+                                
+                                              
+                             </tr>
+                        </tbody>
+                    </Table>
+                </div>
             </div>
 
-            <footer>
-                <h3>Desenvolvido pelo Grupo3</h3>
-            </footer>
-            
+
+            <div className='footerrr p-2 fixed-bottom'>
+                    Desenvolvido por Grupo3
+                </div>
+
         </div >
     )
 }
