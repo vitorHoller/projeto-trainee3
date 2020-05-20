7d9ecf7fb9ee514a16f8b8f4b069416de6f803b7
Antonio Britto <antonio.britto.2000@hotmail.com>
2020-05-20 16:33:06 -0300

    mudanças na pag de listar midias/icone de editar

--- Projeto/grupo3/src/Pages/ListarMidias/listarMidias.js
+++ Projeto/grupo3/src/Pages/ListarMidias/listarMidias.js
@@ -2,11 +2,12 @@ import React from 'react'
 import './listarMidias.css'
 import { Table, Nav, NavDropdown, Navbar, FormControl, Modal, Button } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { FiEdit } from 'react-icons/fi'
 
 function MyVerticallyCenteredModal(props) {
 
     return (
-    
+
         <Modal
             {...props}
             size="lg"
@@ -15,37 +16,39 @@ function MyVerticallyCenteredModal(props) {
         >
 
             <Modal.Header closeButton>
-                
+
                 <Modal.Title id="contained-modal-title-vcenter">
                     La Casa de Papel
+                    <Button className='iconedeEditar d-flex ml-1 btn-sm'>
+                        <FiEdit size={16} />
+                    </Button>
                 </Modal.Title>
-            
-            </Modal.Header>
-          
-            <Modal.Body>
 
-                <h4>Quantidade de Episódios</h4>
+            </Modal.Header>
+
+            <Modal.Body className='topicosdaMidia'>
+
+                <strong>Quantidade de Episódios</strong>
                 <p>38</p>
 
-                <h4>Gênero</h4> 
+                <strong>Gênero</strong>
                 <p>Suspense</p>
 
-                <h4>Data de Lançamento</h4>
+                <strong>Data de Lançamento</strong>
                 <p>01/01/01</p>
 
-                <h4>Tempo de Duração</h4>
+                <strong>Tempo de Duração</strong>
                 <p>45 minutos</p>
 
-                <h4>Descrição</h4>
+                <strong>Descrição</strong>
                 <p>Série muito doida!</p>
 
             </Modal.Body>
 
             <Modal.Footer>
 
-                <Button className = 'mr-5'>Editar</Button>
-                <Button className = 'ml-5 mr-5' onClick={props.onHide}>Close</Button>
-          
+                <Button className='m-auto' onClick={props.onHide}>Fechar</Button>
+
             </Modal.Footer>
 
         </Modal>
@@ -61,7 +64,7 @@ export default function ListarMidias() {
         <div className='back overflow-hidden'>
 
             <div className='headerr rounded'>
-                
+
                 <Navbar className='batbaLM rounded mt-2 mr-2' bg="light" expand="lg">
 
                     <Navbar.Brand>Eu já vi isso!!</Navbar.Brand>
@@ -79,14 +82,14 @@ export default function ListarMidias() {
                     </Navbar.Collapse>
 
                 </Navbar>
-           
+
             </div>
 
-            <div className = 'mt-3'>
+            <div className='mt-3'>
 
                 <h1 className='titulo p-3 pr-5 pl-5 rounded ml-auto mr-auto m-3 mb-4'>Mídias cadastradas</h1>
 
-                <div className = 'tabelaheight mr-auto ml-auto'>
+                <div className='tabelaheight mr-auto ml-auto'>
 
                     <Table striped bordered variant='dark'>
 
@@ -94,48 +97,48 @@ export default function ListarMidias() {
 
                             <tr className='text-body'>
 
-                             <th>#</th>
-                             <th>Nome da Midia</th>
-                             <th>Tipo de Mídia</th>
-                             <th>Detalhes da Mídia</th>
+                                <th>#</th>
+                                <th>Nome da Midia</th>
+                                <th>Tipo de Mídia</th>
+                                <th>Detalhes da Mídia</th>
 
-                             </tr>
+                            </tr>
 
                         </thead>
 
 
                         <tbody className='bodytabela text-center text-dark'>
 
-                             <tr>
+                            <tr>
 
-                                 <td className='numero pt-3'>1</td>
-                                 <td className='pt-3'>La Casa de Papel</td>
-                                 <td className='pt-3'>Série</td>
-                                 
-                                 <>
+                                <td className='numero pt-3'>1</td>
+                                <td className='pt-3'>La Casa de Papel</td>
+                                <td className='pt-3'>Série</td>
 
-                                 <Button 
-                                  variant="outline-light mt-3" 
-                                  className='text-white bg-dark w-75' 
-                                  onClick={() => setModalShow(true)}>
-                                     Clique Aqui
+                                <>
+
+                                    <Button
+                                        variant="outline-light mt-3"
+                                        className='text-white bg-dark w-75'
+                                        onClick={() => setModalShow(true)}>
+                                        Clique Aqui
                                  </Button>
 
-                                 <MyVerticallyCenteredModal
-                                 show={modalShow}
-                                 onHide={() => setModalShow(false)}
-                                 />
-                                 
-                                 </>
-                                           
-                             </tr>
+                                    <MyVerticallyCenteredModal
+                                        show={modalShow}
+                                        onHide={() => setModalShow(false)}
+                                    />
+
+                                </>
+
+                            </tr>
 
                         </tbody>
-                  
+
                     </Table>
 
                 </div>
-            
+
             </div>
 
             <div className='rodapeLM'> Desenvolvido por Grupo3 </div>
